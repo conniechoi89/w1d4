ff6cc525f194f7548b7ae269e8a3bde1581f2484
Connie (Moon Hyung) Choi <32081049+conniechoi89@users.noreply.github.com>
2017-11-02 16:11:25 -0700

    Create advancedSorting.js

---
+++ advancedSorting.js
@@ -0,0 +1,27 @@
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+students.sort(function(a,b) {
+  var nameA = a.name.toUpperCase();
+  var nameB = b.name.toUpperCase();
+
+    if (nameA < nameB) {
+      return -1;
+    }
+
+    if (nameA > nameB) {
+      return 1;
+    }
+
+    if (nameA = nameB) {
+      students["age"] > students["age"];
+        return 1;
+    }
+    return 0;
+});
+
+console.log(students);
